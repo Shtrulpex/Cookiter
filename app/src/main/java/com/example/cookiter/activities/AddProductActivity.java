d3c0a487e1e41de218e8456b9374ae48221dfc55
Shtrulpex <issibirtsev@gmail.com>
2020-06-02 14:29:33 +0300

    1.2

--- app/src/main/java/com/example/cookiter/activities/AddProductActivity.java
+++ app/src/main/java/com/example/cookiter/activities/AddProductActivity.java
@@ -17,6 +17,8 @@ import com.example.cookiter.models.ProductsModel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -41,10 +43,13 @@ public class AddProductActivity  extends AppCompatActivity {
         call.enqueue(new Callback<List<ProductsModel>>() {
             @Override
             public void onResponse(Call<List<ProductsModel>> call, final Response<List<ProductsModel>> response) {
-               String[] s = new String[response.body().size()];
-               for(int i=0;i<s.length; i++){
-                   s[i] = response.body().get(i).getName();
-               }
+                String[] s = new String[response.body().size()];
+                SortedSet<String> sortedSet = new TreeSet<>();
+
+                for(int i=0;i<s.length; i++){
+                       sortedSet.add(response.body().get(i).getName());
+                }
+                sortedSet.toArray(s);
                 ListView lv = findViewById(R.id.listview);
                 ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.list_item, s);
                 lv.setAdapter(adapter);
@@ -56,8 +61,16 @@ public class AddProductActivity  extends AppCompatActivity {
                         ArrayList<String> ls = getIntent().getStringArrayListExtra("ls");
                         ArrayList<Integer> idL = getIntent().getIntegerArrayListExtra("idL");
 
+                        for(int j = 0;j<response.body().size();j++){
+                            if(response.body().get(j).getName().equals(((TextView)view).getText())){
+                                idL.add(response.body().get(j).getId());
+                                Toast.makeText(getApplicationContext(), (response.body().get(j).getId())+"", Toast.LENGTH_SHORT).show();
+                                break;
+                            }
+                        }
+
                         ls.add(((TextView)view).getText().toString());
-                        idL.add(response.body().get((int)d).getId());
+
                         i.putExtra("ls", ls);
                         i.putExtra("idL", idL);
                         i.putExtra("k", true);
